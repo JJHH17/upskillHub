567606d76df1753f39c01f47b1e3003340c69290
James Hatfield <james.hatfield17@icloud.com>
2025-07-16 21:17:19 +0100

    Added method that fetches database credentials for future use

--- src/project/Database.java
+++ src/project/Database.java
@@ -1,8 +1,28 @@
 package project;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Database {
 
     private String dbURL;
     private String dbUsername;
     private String dbPassword;
+
+    public void credentialFetch() {
+        // Pulls from creds file
+        Properties prop = new Properties();
+
+        try (InputStream input = new FileInputStream("db.properties")) {
+            prop.load(input);
+        } catch (Exception e) {
+            System.out.println("There was a problem authenticating");
+            e.printStackTrace();
+        }
+
+        this.dbURL = prop.getProperty("dbURL");
+        this.dbUsername = prop.getProperty("dbUsername");
+        this.dbPassword = prop.getProperty("dbPassword");
+    }
 }
