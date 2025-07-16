efb7ba2e23014b21cd0558764e23d22946197661
James Hatfield <james.hatfield17@icloud.com>
2025-07-16 17:20:10 +0100

    Added test cases for both the size of the knownSkills array and the values of the array

--- src/test/UserTest.java
+++ src/test/UserTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import project.User;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
@@ -13,6 +16,17 @@ public class UserTest {
     @BeforeEach
     void objectCreation() {
         user = new User("testUser", "testPassword", "test@test.com");
+
+        HashMap<String, Integer> skill1 = new HashMap<>();
+        skill1.put("Java", 5);
+        HashMap<String, Integer> skill2 = new HashMap<>();
+        skill2.put("C++", 3);
+        HashMap<String, Integer> skill3 = new HashMap<>();
+        skill3.put("Python", 2);
+
+        user.setKnownSkills(skill1);
+        user.setKnownSkills(skill2);
+        user.setKnownSkills(skill3);
     }
 
     @Test
@@ -27,5 +41,17 @@ public class UserTest {
 
     @Test
     void testEmail() { assertEquals("test@test.com", user.getEmail());
+    }
+
+    @Test
+    void testKnownSkillsSize() {
+        assertEquals(3, user.getKnownSkills().size());
+    }
+
+    @Test
+    void testKnownSkillsValues() {
+        assertEquals(5, user.getKnownSkills().get(0).get("Java"));
+        assertEquals(3, user.getKnownSkills().get(1).get("C++"));
+        assertEquals(2, user.getKnownSkills().get(2).get("Python"));
     }
 }
