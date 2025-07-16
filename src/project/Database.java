cd34d864d6194f81e547601c04de4595d96197c6
James Hatfield <james.hatfield17@icloud.com>
2025-07-16 21:24:41 +0100

    Added method that creates a new table in the database when called

--- src/project/Database.java
+++ src/project/Database.java
@@ -2,6 +2,7 @@ package project;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.sql.*;
 import java.util.Properties;
 
 public class Database {
@@ -11,7 +12,7 @@ public class Database {
     private String dbPassword;
 
     public void credentialFetch() {
-        // Pulls from creds file
+        // Pulls from credential file
         Properties prop = new Properties();
 
         try (InputStream input = new FileInputStream("db.properties")) {
@@ -26,5 +27,23 @@ public class Database {
         this.dbPassword = prop.getProperty("dbPassword");
     }
 
+    public void createTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS users (" +
+                "Username varchar(50) PRIMARY KEY, " +
+                "Password varchar(15) NOT NULL, " +
+                "Email varchar(50) NOT NULL);";
 
+        // Establishing a connection with database
+        try {
+            Connection connection = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+            System.out.println("Table created successfully");
+            connection.close();
+
+        } catch (Exception e) {
+            System.out.println("There was a problem creating the database table");
+            e.printStackTrace();
+        }
+    }
 }
