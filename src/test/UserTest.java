1352d13d59b8bb39f0f2c71a0b929599014a7f56
James Hatfield <james.hatfield17@icloud.com>
2025-07-16 16:33:35 +0100

    Added username and password getter tests

---
+++ src/test/UserTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import project.User;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    void objectCreation() {
+        user = new User("testUser", "testPassword", "test@test.com");
+    }
+
+    @Test
+    void testUsername() {
+        assertEquals("testUser", user.getUsername());
+    }
+
+    @Test
+    void testPassword() {
+        assertEquals("testPassword", user.getPassword());
+    }
+}
