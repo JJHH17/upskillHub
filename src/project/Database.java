33c0eba43c86eed8a51da2e929bd1d3aeb13c8e5
James Hatfield <james.hatfield17@icloud.com>
2025-07-18 11:12:08 +0100

    Readded the SQL qeuries to each method and removed the authentication method, we will refactor this in the future

--- src/project/Database.java
+++ src/project/Database.java
@@ -28,7 +28,12 @@ public class Database {
         this.dbPassword = prop.getProperty("db.password");
     }
 
-    public void tableCreation(String sql) {
+    public void createTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS users (" +
+                "Username varchar(50) PRIMARY KEY, " +
+                "Password varchar(15) NOT NULL, " +
+                "Email varchar(50) NOT NULL);";
+
         // Establishing a connection with database
         try {
             Connection connection = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
@@ -43,19 +48,22 @@ public class Database {
         }
     }
 
-    public void createTable() {
-        String sql = "CREATE TABLE IF NOT EXISTS users (" +
-                "Username varchar(50) PRIMARY KEY, " +
-                "Password varchar(15) NOT NULL, " +
-                "Email varchar(50) NOT NULL);";
-
-        tableCreation(sql);
-    }
-
     public void addUser(User user) {
         String sql = "INSERT INTO users (Username, Password, Email) VALUES (?, ?, ?);";
 
-        tableCreation(sql);
+        try (Connection con = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+             PreparedStatement prepared = con.prepareStatement(sql)) {
+
+            prepared.setString(1, user.getUsername());
+            prepared.setString(2, user.getPassword());
+            prepared.setString(3, user.getEmail());
+            prepared.execute();
+            System.out.println("User added successfully");
+
+        } catch (SQLException e) {
+            System.out.println("There was an error when adding this user");
+            e.printStackTrace();
+        }
     }
 
     public void createDesiredSkillsTable() {
@@ -63,7 +71,18 @@ public class Database {
                 "desired_ID SERIAL PRIMARY KEY, " +
                 "skillName varchar(50) NOT NULL);";
 
-        tableCreation(sql);
+        // Establishing a connection with database // TODO - REMOVE THIS AND REDUCE REUSED CODE
+        try {
+            Connection connection = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+            System.out.println("Table created successfully");
+            connection.close();
+
+        } catch (Exception e) {
+            System.out.println("There was a problem creating the database table");
+            e.printStackTrace();
+        }
     }
 
     public void createKnownSkillsTable() {
@@ -71,7 +90,18 @@ public class Database {
                 + "known_id SERIAL PRIMARY KEY, "
                 + "skillName varchar(50) NOT NULL);";
 
-        tableCreation(sql);
+        // Establishing a connection with database // TODO - REMOVE THIS AND REDUCE REUSED CODE
+        try {
+            Connection connection = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+            System.out.println("Table created successfully");
+            connection.close();
+
+        } catch (Exception e) {
+            System.out.println("There was a problem creating the database table");
+            e.printStackTrace();
+        }
     }
 
     public void userSkillsMapped() {
@@ -82,7 +112,18 @@ public class Database {
                 + "FOREIGN KEY (known_id) REFERENCES knownSkills(known_id), "
                 + " PRIMARY KEY (Username, known_id));";
 
-        tableCreation(sql);
+        // Establishing a connection with database // TODO - REMOVE THIS AND REDUCE REUSED CODE
+        try {
+            Connection connection = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+            System.out.println("Table created successfully");
+            connection.close();
+
+        } catch (Exception e) {
+            System.out.println("There was a problem creating the database table");
+            e.printStackTrace();
+        }
     }
 
     public void desiredSkillsMapped() {
@@ -93,6 +134,16 @@ public class Database {
                 + "FOREIGN KEY (desired_ID) REFERENCES desiredSkills(desired_ID), "
                 + " PRIMARY KEY (Username, desired_ID));";
 
-        tableCreation(sql);
+        try {
+            Connection connection = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+            System.out.println("Table created successfully");
+            connection.close();
+
+        } catch (Exception e) {
+            System.out.println("There was a problem creating the database table");
+            e.printStackTrace();
+        }
     }
 }
