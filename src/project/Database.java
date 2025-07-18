178ff2589cb0745d63d42ab7c0731f9831b79683
James Hatfield <james.hatfield17@icloud.com>
2025-07-18 11:03:19 +0100

    Added method that creates a new table to map users to their desired skills

--- src/project/Database.java
+++ src/project/Database.java
@@ -125,4 +125,25 @@ public class Database {
             e.printStackTrace();
         }
     }
+
+    public void desiredSkillsMapped() {
+        String sql = "CREATE TABLE IF NOT EXISTS knownSkillsMapped ("
+                + "Username varchar(50) NOT NULL, "
+                + "desired_ID INTEGER NOT NULL, "
+                + "FOREIGN KEY (Username) REFERENCES users(Username), "
+                + "FOREIGN KEY (desired_ID) REFERENCES desiredSkills(desired_ID), "
+                + " PRIMARY KEY (Username, desired_ID));";
+
+        try {
+            Connection connection = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+            System.out.println("Table created successfully");
+            connection.close();
+
+        } catch (Exception e) {
+            System.out.println("There was a problem creating the database table");
+            e.printStackTrace();
+        }
+    }
 }
