196aedee8aeeed8a19a83d1015611f89ff8123d6
James Hatfield <james.hatfield17@icloud.com>
2025-07-20 12:22:04 +0100

    Added JavaFX template which now loads a new stage when executed

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,6 +1,34 @@
 package project;
 
-public class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Button button;
+
     public static void main(String[] args) {
+        launch(args);
+
+        // UserInterface userInterface = new UserInterface();
+        // userInterface.start();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("UpskillHub");
+        button = new Button("Test button");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        stage.setScene(scene);
+        stage.show();
     }
 }
